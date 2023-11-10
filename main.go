cadbedb0b55c8ef8f4808c54c5b4a3619bb8afc2
Sergey Sviridenko <s.sviridenko05@bk.ru>
2023-11-10 14:15:10 +0500

    add post method & add get one book method

--- main.go
+++ main.go
@@ -27,8 +27,33 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func getBook(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, b := range books {
+		if b.ID == id {
+			c.IndentedJSON(http.StatusOK, b)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Книга не найдена"})
+}
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.GET("/books/:id", getBook)
+	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
